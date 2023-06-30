9f53b634aaccafcbf2992dd796385ad2e702bc0e
ST10366411 <128060233+tabassumally@users.noreply.github.com>
2023-06-30 20:26:28 +0200

    Add files via upload

---
+++ ST10366411-WEDE5020-POE-Part 3-Tabassum Ally/script.js
@@ -0,0 +1,69 @@
+'use strict';
+
+
+
+/**
+ * navbar toggle
+ */
+
+const navOpenBtn = document.querySelector("[data-nav-open-btn]");
+const navbar = document.querySelector("[data-navbar]");
+const navCloseBtn = document.querySelector("[data-nav-close-btn]");
+
+const navElemArr = [navOpenBtn, navCloseBtn];
+
+for (let i = 0; i < navElemArr.length; i++) {
+  navElemArr[i].addEventListener("click", function () {
+    navbar.classList.toggle("active");
+  });
+}
+
+/**
+ * toggle navbar when click any navbar link
+ */
+
+const navbarLinks = document.querySelectorAll("[data-nav-link]");
+
+for (let i = 0; i < navbarLinks.length; i++) {
+  navbarLinks[i].addEventListener("click", function () {
+    navbar.classList.remove("active");
+  });
+}
+
+
+
+
+
+/**
+ * header active when window scrolled down
+ */
+
+const header = document.querySelector("[data-header]");
+
+window.addEventListener("scroll", function () {
+  window.scrollY >= 50 ? header.classList.add("active")
+    : header.classList.remove("active");
+});
+
+/*
+ *contact page
+*/
+
+const inputs = document.querySelectorAll(".input");
+
+function focusFunc() {
+  let parent = this.parentNode;
+  parent.classList.add("focus");
+}
+
+function blurFunc() {
+  let parent = this.parentNode;
+  if (this.value == "") {
+    parent.classList.remove("focus");
+  }
+}
+
+inputs.forEach((input) => {
+  input.addEventListener("focus", focusFunc);
+  input.addEventListener("blur", blurFunc);
+});
